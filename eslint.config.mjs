b497944f2bc00d999828dae23c89cf7cc34098a1
Davide Ciulla <davide.ciulla@hotmail.com>
2024-08-25 14:59:43 +0200

    Reinstalled eslint-plugin-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from '@eslint/js';
 import ts from 'typescript-eslint';
 import globals from 'globals';
-import prettier from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default [
   {
